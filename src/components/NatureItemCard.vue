818e3b53a1fa268e5fca7a2a273bf9ffa7950a4a
Lauren Stephenson <CompSciLauren@gmail.com>
2021-08-08 22:06:24 -0500

    Add NatureItemCard

---
+++ src/components/NatureItemCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="natureItemCard">
+      <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NatureItemCard',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+    font-size: 28px;
+}
+
+.natureItemCard {
+  color: #42b983;
+  border: #43b983 solid 2px;
+  width: 50%;
+}
+</style>
---
+++ src/components/NatureItemCardContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="natureItemCardContainer">
+    <NatureItemCard title="Snakes"/>
+    <NatureItemCard title="Lizards"/>
+    <NatureItemCard title="Frogs"/>
+    <NatureItemCard title="Spiders"/>
+    <NatureItemCard title="Birds"/>
+    <NatureItemCard title="Plants"/>
+  </div>
+</template>
+
+<script>
+import NatureItemCard from '@/components/NatureItemCard.vue'
+
+export default {
+  name: 'NatureItemCardContainer',
+  props: {
+    title: String
+  },
+  components: {
+    NatureItemCard
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.natureItemCardContainer {
+  display: flex;
+  flex-wrap: wrap;
+  color: #42b983;
+  border: black solid 2px;
+  width: 40%;
+  margin: auto;
+}
+</style>
